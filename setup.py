737d03c120daa451038e605811c3aa3d1d245abe
Tom Catshoek <t.w.j.catshoek@student.tudelft.nl>
2021-02-04 12:02:22 +0100

    release 0.0.2

--- setup.py
+++ setup.py
@@ -16,8 +16,14 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-        "Operating System :: OS Independent",
+        "Operating System :: POSIX :: Linux",
         "Development Status :: 3 - Alpha",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'tabulate',
+        'graphviz',
+        'pygtrie',
+    ]
 )
