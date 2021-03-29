b3e9187d9c268c28dab3ee3a13a80366031585c6
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2021-03-29 16:26:43 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="stmlearn",
-    version="0.0.2",
+    version="0.0.3",
     author="T. Catshoek",
     author_email="tomcatshoek@zeelandnet.nl",
     description="Python state machine learning library",
